de779bef415fd4ce694a8e2124cff051a27a2e10
jvroot <joaovlima.delima@gmail.com>
2024-08-01 12:21:32 -0300

    att doc

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,8 @@
+/**
+ * Terraform configuration file for creating a VPC, subnet, internet gateway, route table,
+ * security group, EC2 instances, and Elastic IPs in AWS.
+ */
+
 terraform {
     required_providers {
       aws = {
@@ -7,15 +12,12 @@ terraform {
     }
 }
 
-
-# Configure the AWS Provider
+// Configure the AWS Provider
 provider "aws" {
     region = "us-east-1"
 }
 
-
-
-# Criação da VPC, subnet, gateway de internet e tabela de rotas
+// Create the VPC, subnet, internet gateway, and route table
 resource "aws_vpc" "Jenkins_Meter_VPC" {
   cidr_block = "10.0.0.0/16"
 }
@@ -43,7 +45,7 @@ resource "aws_route_table_association" "Jenkins_Meter_route_table_association" {
   route_table_id = aws_route_table.Jenkins_Meter_route_table.id
 }
 
-# Criação do security group
+// Create the security group
 resource "aws_security_group" "Jenkins_Meter_security_group" {
   name        = "Jenkins_Meter_security_group"
   description = "Allow all traffic"
@@ -64,11 +66,10 @@ resource "aws_security_group" "Jenkins_Meter_security_group" {
   }
 }
 
-# ami-04a81a99f5ec58529 é a AMI do Ubuntu 20.04 LTS
-
+// Create the Jenkins Meter EC2 instance
 resource "aws_instance" "Jenkins_Meter_Server" {
   ami           = "ami-03507b3027526ccc3"
-  instance_type = "t2.medium"
+  instance_type = "t2.large"
   key_name      = "Jenkins_Server"
 
   subnet_id     = aws_subnet.Jenkins_Meter_Subnet.id
@@ -84,29 +85,26 @@ resource "aws_instance" "Jenkins_Meter_Server" {
   }
 }
 
-# Criação Elastic IP
+// Create the Elastic IP for Jenkins Meter EC2 instance
 resource "aws_eip" "JMeter_Server_eip" {
   vpc = true
 }
 
-# Associação do Elastic IP à instância EC2
+// Associate the Elastic IP with the Jenkins Meter EC2 instance
 resource "aws_eip_association" "Jenkins_Meter_Server_eip_association" {
   instance_id   = aws_instance.Jenkins_Meter_Server.id
   allocation_id = aws_eip.JMeter_Server_eip.id
 }
 
-# Recurso nulo apenas para acionar a criação da instância EC2 e associação do Elastic IP
+// Create a null resource to trigger the creation of the Jenkins Meter EC2 instance and Elastic IP association
 resource "null_resource" "Jenkins_Meter_Server_trigger" {
   depends_on = [aws_instance.Jenkins_Meter_Server]
 }
 
-
-
-
-# Criação da instância EC2 - Aplicação Easy Travel
+// Create the Easy Travel EC2 instance
 resource "aws_instance" "Easy_Travel_Instance" {
   ami           = "ami-0ac2886caf2877ccd"
-  instance_type = "t2.small"
+  instance_type = "t2.medium"
   key_name = "Jenkins_Server"
 
   subnet_id     = aws_subnet.Jenkins_Meter_Subnet.id
@@ -117,38 +115,34 @@ resource "aws_instance" "Easy_Travel_Instance" {
     volume_size = 30
   }
 
-
   tags = {
     Name = "Easy_Travel_instance"
   }
-
 }
 
-
-
-# Criação do Elastic IP
+// Create the Elastic IP for Easy Travel EC2 instance
 resource "aws_eip" "Easy_Travel_eip" {
   vpc = true
 }
 
-# Associação do Elastic IP à instância EC2
+// Associate the Elastic IP with the Easy Travel EC2 instance
 resource "aws_eip_association" "Easy_Travel_eip_association" {
   instance_id   = aws_instance.Easy_Travel_Instance.id
   allocation_id = aws_eip.Easy_Travel_eip.id
 }
 
+// Create a null resource to trigger the creation of the Easy Travel EC2 instance and Elastic IP association
 resource "null_resource" "Easy_Travel_trigger" {
   depends_on = [aws_instance.Easy_Travel_Instance]
 }
 
-
-# Outputs
+// Outputs
 output "easy_travel_elastic_ip" {
   description = "The Elastic IP address of the Easy Travel EC2 instance"
   value       = aws_eip.Easy_Travel_eip.public_ip
 }
 
 output "jenkins_server_elastic_ip" {
-  description = "The Elastic IP address of the Second EC2 instance"
+  description = "The Elastic IP address of the Jenkins Meter EC2 instance"
   value       = aws_eip.JMeter_Server_eip.public_ip
 }
